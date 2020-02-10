7262766648a5adde2b15a130e56dc7f24207ad09
jgrenat <jgrenat@users.noreply.github.com>
2020-02-10 20:51:58 +0100

    Add article 'Types Top 2'

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import typescript from "highlight.js/lib/languages/typescript.js";
 import javascript from "highlight.js/lib/languages/javascript.js";
 import java from "highlight.js/lib/languages/java.js";
 import bash from "highlight.js/lib/languages/bash.js";
+import haskell from "highlight.js/lib/languages/haskell.js";
+import idris from "highlight.js/lib/languages/ocaml.js";
 import "highlight.js/styles/dracula.css";
 hljs.registerLanguage('elm', elm);
 hljs.registerLanguage('rust', rust);
@@ -18,6 +20,8 @@ hljs.registerLanguage('typescript', typescript);
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('bash', bash);
 hljs.registerLanguage('java', java);
+hljs.registerLanguage('haskell', haskell);
+hljs.registerLanguage('idris', idris);
 
 const { Elm } = require("./src/Main.elm");
 const pagesInit = require("elm-pages");
